8c45aed139278b822b19aac75c22e01b2272bbeb
ilMitch <ilmitch.py@gmail.com>
2020-03-15 17:01:15 +0100

    2020/03/15 ecdc data update

--- frontend/working/main.py
+++ frontend/working/main.py
@@ -10,60 +10,88 @@ idx = pd.IndexSlice
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 
-w_df_filepath = "../../backend/working/_output/pckl/covid_df.pckl"
+df_filepath = "../../backend/working/_output/pckl/covid_df.pckl"
 
 app = dash.Dash()
 
 app.layout = html.Div([
     html.H1('COVID-19 | Explorative Data Visualization'),
+
     html.Div([
         html.H3('Enter a country name:', style={'paddingRight':'30px'}),
         dcc.Input(
-            id='my_data_picker', 
+            id='my_country_picker', 
             value='Switzerland',
-            style={'fontSize':16,'width':75})
+            style={'fontSize':24,'width':100})
             ],
         style={'display':'inline-block', 'verticalAlign':'top'}),
+
     html.Div([
         html.H3('Select a start and end date:'),
         dcc.DatePickerRange(
             id='my_date_picker',
-            min_date_allowed=date(2020,1,1),
+            min_date_allowed=date(2019,12,31),
             max_date_allowed=date.today(),
-            )
-        ]),
+            start_date=date(2020,1,1),
+            end_date=date.today() 
+            )], 
+        style={'display':'inline-block'}),
+
+    html.Div([
+        html.Button(
+            id='submit-button',
+            n_clicks=0,
+            children='Submit',
+            style={'fontSize':16, 'marginLeft':'30px'}
+            )],
+        style={'display':'inline-block'}),
+
     dcc.Graph(
         id='my_graph', 
         figure={
-            'title':'Total Confirmed Cases',
-            'data':[{'x':[1,2], 'y':[3,1]}],
-            'layout' : [{'title':'Total Confirmed Cases (daily cumulative sum)'}]
+            'data':[{'x':[1,2], 'y':[3,1]}]
             }
         )
     ])
 
 
-@app.callback(Output('my_graph', 'figure'),[Input('my_data_picker','value')])
-def update_graph(country_ticker):
-    start = date(2020,1,1)
-    end = date.today()
-    df = pd.read_pickle(w_df_filepath)
+@app.callback(
+    Output('my_graph', 'figure'),
+    [
+        Input('submit-button', 'n_clicks')
+        ],
+    [
+        State('my_country_picker', 'value'),
+        State('my_date_picker', 'start_date'),  
+        State('my_date_picker', 'end_date')
+    ])
+
+def update_graph(n_clicks, country_ticker, start_date, end_date):
+    '''
+    Update plot data for specified country and date range
+    '''
+    start = dt.datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = dt.datetime.strptime(end_date[:10], '%Y-%m-%d')
+
+    df = pd.read_pickle(df_filepath)
     #c_df = df.loc[idx['Switzerland',:],idx['cumsum','NewConfCases']]
     # Slicing data per input country
-    c_df = df.loc[idx[country_ticker,:],idx['cumsum','NewConfCases']]
-    
-    fig = {'data':[{'x':list(c_df.index.levels[1]), 'y':list(c_df.values)}],
-           'layout': go.Layout(
-                yaxis={
-                    'title': 'Total Confirmed Cases',
-                    'type': 'log'
-                      },
-            #margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
-        )}
+    c_df = df.loc[idx[country_ticker,start:end],idx['cumsum','NewConfCases']]
+    dates_lst = [item[1] for item in c_df.index] #inelegant but effective way to extract the dates from the sliced df
+    fig = {
+            'data':[{'x':dates_lst, 'y':list(c_df.values)}],
+            'layout': 
+                go.Layout(
+                    yaxis={
+                        'title': 'Total Confirmed Cases',
+                        'type': 'log'
+                        },
+                    #margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+                    hovermode='closest'
+            )}
     return fig
 
 
