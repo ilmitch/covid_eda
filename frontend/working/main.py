1bfd413d6908a7247d06888984c6f73bedc69dc8
ilMitch <ilmitch.py@gmail.com>
2020-03-15 12:50:42 +0100

    main.py basic visualization

--- frontend/working/main.py
+++ frontend/working/main.py
@@ -11,6 +11,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 w_df_filepath = "../../backend/working/_output/pckl/covid_df.pckl"
 
@@ -24,8 +25,9 @@ app.layout = html.Div([
 
                     dcc.Graph(id='my_graph', 
                               figure={
+                                  'title':'Total Confirmed Cases',
                                   'data':[{'x':[1,2], 'y':[3,1]}],
-                                  'layout' : {'title':'Default Title'}
+                                  'layout' : [{'title':'Total Confirmed Cases (daily cumulative sum)'}]
                                   }
                               )
                     ])
@@ -36,10 +38,19 @@ def update_graph(country_ticker):
     start = date(2020,1,1)
     end = date.today()
     df = pd.read_pickle(w_df_filepath)
-    c_df = df.loc[idx['Switzerland',:],idx['cumsum','NewConfCases']]
-    fig = {'data':[{'x':list(df.index.levels[1]), 'y':list(c_df.values)}],
-           'layout' : {'title':country_ticker}
-           }
+    #c_df = df.loc[idx['Switzerland',:],idx['cumsum','NewConfCases']]
+    # Slicing data per input country
+    c_df = df.loc[idx[country_ticker,:],idx['cumsum','NewConfCases']]
+    
+    fig = {'data':[{'x':list(c_df.index.levels[1]), 'y':list(c_df.values)}],
+           'layout': go.Layout(
+                yaxis={
+                    'title': 'Total Confirmed Cases',
+                    'type': 'log'
+                      },
+            #margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )}
     return fig
 
 
